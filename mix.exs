5cb6d506cd67b505c53ea338b7d7d736638e5e69
Sean Cribbs <seancribbs@gmail.com>
2020-03-17 09:49:08 -0500

    Add hex metadata

--- mix.exs
+++ mix.exs
@@ -4,12 +4,26 @@ defmodule OpencensusEcto.MixProject do
   def project do
     [
       app: :opencensus_ecto,
+      description: description(),
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
+      source_url: "https://github.com/seancribbs/opencensus_ecto"
+    ]
+  end
+
+  defp description do
+    "Trace Ecto queries with Opencensus."
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/seancribbs/opencensus_ecto"}
     ]
   end
 
@@ -28,6 +42,7 @@ defmodule OpencensusEcto.MixProject do
     [
       {:telemetry, "~> 0.4.0"},
       {:opencensus, "~> 0.9.0"},
+      {:ex_doc, "~> 0.21.0", only: [:dev], runtime: false},
       {:ecto_sql, ">= 3.0.0", only: [:test]},
       {:postgrex, ">= 0.15.0", only: [:test]}
     ]
